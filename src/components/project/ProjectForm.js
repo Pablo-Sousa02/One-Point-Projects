3642ff64db36f4fcfadd524265765da778d537b3
Pablo-Sousa02 <pablooficial22@hotmail.com>
2024-11-12 22:04:11 -0300

    formulario adicionado

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,22 +1,27 @@
-function ProjectForm() {
+import Input from '../form/Input'; // Importando Input
+import Select from '../form/Select'; // Importando Select
+import styles from './ProjectForm.module.css'; // Estilos para o ProjectForm
+import SubmitButton from '../form/SubmitButton'; // Importando o componente SubmitButton
+
+function ProjectForm({ btnText }) {
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto " />
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total" />
-            </div>
-            <div>
-                <select name="category_id">
-                    <option disabled>Selecione a categoria</option>
-                </select>
-            </div>
-            <div>
-                <input type = "submit" value="Criar projeto"/>
-            </div>
+        <form className={styles.form}>
+            <Input
+                type="text"
+                text="Nome do Projeto"
+                name="name"
+                placeholder="Insira o nome do Projeto"
+            />
+            <Input
+                type="number"
+                text="Orçamento do Projeto"
+                name="budget"
+                placeholder="Insira o orçamento total"
+            />
+            <Select name="category_id" text="Selecione uma categoria" />
+            <SubmitButton text={btnText} />
         </form>
-    )
+    );
 }
 
-export default ProjectForm
+export default ProjectForm;
