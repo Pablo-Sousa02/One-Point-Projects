3cfe11fbc34e096f841dea7b18f49c6cd706cbf2
Pablo-Sousa02 <pablooficial22@hotmail.com>
2024-11-13 21:24:20 -0300

    db.json adicionado para as options  de criar projeto

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react'
+
 import Input from '../form/Input'; // Importando Input
 import Select from '../form/Select'; // Importando Select
 import styles from './ProjectForm.module.css'; // Estilos para o ProjectForm
 import SubmitButton from '../form/SubmitButton'; // Importando o componente SubmitButton
 
 function ProjectForm({ btnText }) {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input
@@ -18,7 +36,10 @@ function ProjectForm({ btnText }) {
                 name="budget"
                 placeholder="Insira o orçamento total"
             />
-            <Select name="category_id" text="Selecione uma categoria" />
+            <Select
+                name="category_id"
+                text="Selecione uma categoria"
+                options={categories} />
             <SubmitButton text={btnText} />
         </form>
     );
